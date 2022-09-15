038997b07f6ee3e3e3e6395ba96ee1863b675fb6
Alvaro <alvaro@baut.bm>
2022-09-15 15:30:24 +0200

    process_propertyskipper.py

--- process_propertyskipper.py
+++ process_propertyskipper.py
@@ -1,7 +1,7 @@
 # process property skipper data
 
 import configparser
-import utils.skipperutils as sku
+import utils.skipperutils as skipu
 
 # Get secret URL API
 keys = configparser.ConfigParser()
@@ -9,10 +9,24 @@ keys.read("./utils/kw_config.txt")
 url = keys.get("skipper", "URL")
 
 # download XML and convert to CSV
-csv_data = sku.download_skipper_xml(url)
-
+csv_data = skipu.download_skipper_xml(url)
+print("\n Last XML downloaded and saved to ./data/skipper/ \n")
 # note that we neeed a strategy to clear up old
 # CSV files as they will start taking up space (~1.5MB each)
 
-clean_and_flag_properties(csv_data)
+# flag properties with bad address, assessment number, country
+df = skipu.clean_and_flag_properties(csv_data)
+
+
+# make property type uniform
+df = skipu.uniform_property_type(df)
+
+# add land or fractional to property_type if appropriate
+df = skipu.identify_land_and_fractional(df)
+print("properties cleaned, property_type identified. FINISHED.\n\n")
+
+# to do: 
+# - identify land
+# - reverse order (clean and flag, after idenfity land and fractional which are not flagged)
+ 
 
--- utils/skipperutils.py
+++ utils/skipperutils.py
@@ -84,12 +84,93 @@ def download_skipper_xml(url):
     os.remove(datafile)
     return csvdata
 
+
+def _fractional_filter(df):
+    """
+    function to filter dataframe searching for fractional properties
+    """
+    if pd.isnull(df['property_type']):
+        return 0
+    elif ('fractional' in str(df.name)):
+        return 'fractional'
+    elif (("Tucker's Point" in str(df.name)) | ("Newstead" in str(df['name'])) |
+   ("Reefs" in str(df.name)))  & (df['property_type'] == 'condo'):
+        return 'fractional'
+    else:
+        return df['property_type']
+
+def identify_land_and_fractional(df):
+    """
+    Find properties which may have the wrong property type
+    In particular those which don't have assessment number
+    because they were actually land or fractional
+    """
+    df.property_type = df.apply(_fractional_filter, axis = 1)
+    return df
+
+
 def contains_number(value):
     ''' check if the value has numbers '''
     return bool(re.findall('[0-9]+', value))
-    
-def clean_and_flag_properties(csv):
 
-    # create "data frame property skipper" dfps
+def clean_and_flag_properties(csv):
+    '''
+    Create a dataframe from the csv file and then,
+    Flag properties:
+    - without assessment number
+    - without address
+    - with the wrong country
+    '''
     dfps = pd.read_csv(csv)
     flag_list = []
+    for index, row in dfps.iterrows():
+        an = row.assessment_number
+        nam = str(row["name"])
+        
+        if type(an) == float:
+            # catch assessment_numver == nan
+            flag_list.append("FLAG - Assn #")
+        elif (len(an) == 8) or (len(an)== 9):
+            # has a valid assessment number
+            if (contains_number(nam)) and (len(nam) > 20):
+                # probably has a valid address
+                # at least it is long and has a number
+                flag_list.append(0) # 0 = no flag
+            elif str(an) == "000000000" or str(an) == "00000000":
+                flag_list.append("FLAG - Assn #")
+            else:
+                # if it either
+                # - has a number but is short
+                # - has no number
+                # it probably needs to be reviewed
+                flag_list.append("FLAG - Addr")
+                
+        elif row["country"].lower() != "bermuda":
+            flag_list.append("FLAG - Country")
+        else:
+            flag_list.append("FLAG - Assn #")
+    dfps["flag"] = flag_list
+
+    # Currently in bermuda we find all "is_let = 0".
+    # in case rent or let exist at the same time or separately:
+    rent_or_let = dfps["is_let"] + dfps["is_rent"]
+    rent_or_let.apply(lambda x: 0 if x < 0 else 1)
+    dfps["is_rent"] = rent_or_let  # now can be 0=not_rent or 1=rent
+    return dfps
+
+def uniform_property_type(df):
+    """
+    Map the diversity of property types to only a few categories
+    Identify and label the fractional properties
+    """
+    # Load Dictionary from CSV
+    with open('data/property_type_dict.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        property_type_dict = dict(reader)
+    # map some property types
+    df["property_type"] = df["property_type"].str.lower()
+    df2=df.replace({"property_type": property_type_dict})
+
+    #
+    return df2
+
