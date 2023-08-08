d19a068d595b8e3a48b2da82f5414b270f56cb67
dersu <alphydan@gmail.com>
2023-08-08 11:10:55 -0300

    refactor/fix(landval): change in criterion for accepting properties with address length. Now accept 12 characters or more instead of 20 characters of more as warranted by the data.  Also better doc for the files processing landvaluation data

--- process_landvaluation.py
+++ process_landvaluation.py
@@ -6,7 +6,15 @@ import pandas as pd
 import utils.LTROutils as LT
 import utils.landvalutils as LAV
 
-# Have new properties been added by scraping?
+#  Importing data from land valuation has the following steps:
+# 1. check if there is new data that has been scraped
+# 2. If so, integrate it with the old data
+# 3. process duplicates
+# 4. use standard property_type
+# 5. use standard parishes
+
+
+# 1. Have new properties been added by scraping?
 # If so, add them to the latest_landvaluation_data.csv file
 # concatenate the two csv files and then remove duplicates
 
@@ -16,18 +24,18 @@ last_scraped_data = max(files_from_scraping, key=os.path.getctime)
 
 latest_lv_data = "./data/landvaluation/latest_landvaluation_data.csv"
 
-# Import both files and concatenate them
+# 2. Import both files and concatenate them
 df = pd.concat([pd.read_csv(latest_lv_data), pd.read_csv(last_scraped_data)], ignore_index=True)
 
-
-df = df.drop_duplicates()
+# 3. Process Duplicates (also further below)
 ##### change to lower
 df["property_type"] = df["property_type"].str.lower().str.strip()
 df["tax_code"] = df["tax_code"].str.lower().str.strip()
 df["address_low"] = df["address"].str.lower().str.strip()
 df["building_name_low"] = df["building_name"].str.lower().str.strip()
+df = df.drop_duplicates()
 
-##### Simplify Categories
+# 4. Simplify Categories (Standardise property_type)
 # Load Dictionary from CSV
 with open('./data/property_type_dict.csv') as csv_file:
     reader = csv.reader(csv_file)
@@ -36,7 +44,7 @@ with open('./data/property_type_dict.csv') as csv_file:
 # Map new categories using the dictionary    
 df2=df.replace({"property_type": property_type_dict})
 df2["property_type"].value_counts() # the last one is currently "land"
-len(df2["property_type"].value_counts()) # there are currently 19 property_type
+print("\n", len(df2["property_type"].value_counts()), "property types identified.\n") # there are currently 12 property_type
 
 
 ##### change ARV to numbers
@@ -64,8 +72,10 @@ if mxarv > 10000000:
 else:
     print("Min ARV:", minarv, "and", "Max ARV", mxarv,  "[OK]")
 
+# 5. Simplify Parishes
 df2 = LT.simplify_parishes(df2)
-    
+
+# Select columns of interest    
 df_for_export = df2[["assn_nr","arv","tax_code","property_type", "address", "grid", "parish", "building_name"]]
 # save to CSV
 df_for_export.to_csv("./data/kw-properties.csv", index=False)
